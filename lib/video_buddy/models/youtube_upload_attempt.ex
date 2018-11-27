bf62777b94b97a1346d215c304ce8a93be69c498
Trey Evans <tevans@MacBook-Pro-3.local>
2018-11-27 18:05:48 -0500

    Auto set file size.

--- lib/video_buddy/models/youtube_upload_attempt.ex
+++ lib/video_buddy/models/youtube_upload_attempt.ex
@@ -2,6 +2,8 @@ defmodule VideoBuddy.Models.YoutubeUploadAttempt do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @already_on_youtube_file_location "PREVIOUS_YOUTUBE_UPLOAD"
+
   schema "youtube_upload_attempts" do
     field :title, :string
     field :description, :string, default: ""
@@ -34,10 +36,10 @@ defmodule VideoBuddy.Models.YoutubeUploadAttempt do
            :tags,
            :uploading_uri,
            :upload_progress,
-           :file_size,
            :youtube_video_id,
            :publish_at,
-           :visibility
+           :visibility,
+           :file_size
         ])
       |> validate_required([
            :title,
@@ -52,6 +54,22 @@ defmodule VideoBuddy.Models.YoutubeUploadAttempt do
           ["unlocked", "not_yet_attempted", "claimed_but_unstarted", "upload_uri_set", "uploading", "interrupted", "upload_complete"]
        )
       |> validate_publish_at_if_scheduled
+      |> validate_file_location_and_set_size
+  end
+
+  defp validate_file_location_and_set_size(changeset) do
+    case fetch_field(changeset, :source_file_location) do
+      {_, nil} -> changeset # already done by required constraint
+      {_, @already_on_youtube_file_location} -> changeset
+      {_, loc_value} -> check_file_size(changeset, loc_value)
+    end
+  end
+
+  defp check_file_size(changeset, file_location) do
+    case File.stat(file_location) do
+      {:ok, %{size: c_len}} -> change(changeset, file_size: c_len)
+      _ -> add_error(changeset, :source_file_location, "is not a valid file location")
+    end
   end
 
   defp validate_publish_at_if_scheduled(changeset) do
@@ -70,5 +88,9 @@ defmodule VideoBuddy.Models.YoutubeUploadAttempt do
       {:data, _} -> changeset
       _ -> add_error(changeset, :publish_at, "can't be blank")
     end
+  end
+
+  def already_on_youtube_location() do
+    @already_on_youtube_file_location
   end
 end
